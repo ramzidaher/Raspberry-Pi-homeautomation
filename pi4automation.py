b4ea999ee9c81845cbe4fe10a1172afe0675a657
ramzidaher <68782812+ramzidaher@users.noreply.github.com>
2021-12-18 20:37:41 +0000

    Create pi4automation.py
    I used two Pi's a Pi4 and a Pi Zero each for different places in the room, so I made two webserver's each running on their own, usually on startup.

---
+++ pi4automation.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+from time import sleep
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('light_index.html')
+@app.route("/<deviceName>/")
+def action(deviceName):
+    if deviceName == 'off' :
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(11, GPIO.OUT)
+        pwm=GPIO.PWM(11, 50)
+        pwm.start(0)
+        pwm.ChangeDutyCycle(5)
+        sleep(0.1)
+        pwm.stop()
+        GPIO.cleanup()
+        GPIO.setwarnings(False)
+        return render_template ('light_index.html')
+
+    if deviceName == 'on' :
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(11, GPIO.OUT)
+        pwm=GPIO.PWM(11,50)
+        pwm.start(0)
+        pwm.ChangeDutyCycle(1.5)
+        sleep(0.11)
+        pwm.stop()
+        GPIO.cleanup()
+        GPIO.setwarnings(False)
+        return render_template ('light_index.html')
+        
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=50, debug=True)
+
